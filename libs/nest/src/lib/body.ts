0513d5ad582c857b89f9c309526d8f5bb3f8b45f
robert-brightline <robert.brightline@gmail.com>
2026-02-09 21:54:34 -0600

    feat: created the propety library for validation and swagger and tested it

--- libs/nest/src/lib/body.ts
+++ libs/nest/src/lib/body.ts
@@ -1,7 +1,7 @@
 import { Body as NestBody } from "@nestjs/common";
 import type { Cls } from "@vnodes/types";
 
-export function Body(dtoClass?: Cls): ParameterDecorator {
+export function Body(_dtoClass?: Cls): ParameterDecorator {
     return (...args) => {
         NestBody()(...args);
     };
