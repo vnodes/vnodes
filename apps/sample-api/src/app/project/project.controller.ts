08028245bcefa70ac3ad89dcb0fddc1f6bc52943
robert-brightline <robert.brightline@gmail.com>
2026-03-02 18:38:43 -0600

    feat: working on dto generation

--- apps/sample-api/src/app/project/project.controller.ts
+++ apps/sample-api/src/app/project/project.controller.ts
@@ -1,7 +1,6 @@
-import { BaseProjectController, QueryMany } from '@vnodes/dbs/pms';
+import { BaseProjectController } from '@vnodes/dbs/pms';
 import { CrudController } from '@vnodes/nestjs';
 import { Inject } from '@vnodes/nestjs/common';
-import { ProjectScalarFieldEnum } from 'node_modules/@vnodes/dbs/dist/pms/src/internal/prismaNamespace.js';
 import { ProjectCreateDto, ProjectQueryDto, ProjectReadDto, ProjectUpdateDto } from './project.dto.js';
 import { ProjectService } from './project.service.js';
 
@@ -14,19 +13,5 @@ import { ProjectService } from './project.service.js';
 export class ProjectController extends BaseProjectController {
     constructor(@Inject(ProjectService) service: ProjectService) {
         super(service);
-    }
-
-    override findMany(query: QueryMany<ProjectScalarFieldEnum>): Promise<
-        {
-            id: number;
-            createdAt: Date;
-            updatedAt: Date;
-            deletedAt: Date | null;
-            name: string;
-            description: string | null;
-        }[]
-    > {
-        console.table(query);
-        return super.findMany(query);
     }
 }
--- apps/sample-api/src/app/project/project.dto.ts
+++ apps/sample-api/src/app/project/project.dto.ts
@@ -10,6 +10,10 @@ export class ProjectUpdateDto extends PartialType(ProjectCreateDto) {}
 
 export class ProjectReadDto implements Project {
     @Prop() id: number;
+
+    @Prop() uuid: string;
+    @Prop() count: number | null;
+    @Prop() counts: number[];
     @Prop() name: string;
     @Prop() description: string;
     @Prop() createdAt: Date;
