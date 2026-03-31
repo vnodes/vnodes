e0e6b655164402fde9800e4ff083d29a3c78f3c2
robert-brightline <robert.brightline@gmail.com>
2026-03-30 19:41:53 -0500

    feat: tested gRpc client generator

--- apps/sample-service/scripts/build.mjs
+++ apps/sample-service/scripts/build.mjs
@@ -1,25 +1,22 @@
 import { execSync } from "node:child_process";
-import { join } from "node:path";
-
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-
-console.log('building');
-const plugin = join('..', 'node_modules/.bin/protoc-gen-ts_proto')
-
-const protodir = join('..', 'proto')
-const protofile = join('..', 'proto', 'sample-service.proto')
+const plugin = join(__dirname,'..', 'node_modules/.bin/protoc-gen-ts_proto')
+const protodir = join(__dirname,'..', 'proto')
+const protofile = join(__dirname,'..', 'proto', 'sample-service.proto')
+const output = join(__dirname, '..', 'src/generated')
 
 execSync(`
     protoc \
-  --plugin= ${plugin}\
-  --ts_proto_out=./src/generated \
+  --plugin="${plugin}"\
+  --ts_proto_out="${output}" \
   --ts_proto_opt=nestJs=true \
-  --ts_proto_opt=fileSuffix=.ts \
   --ts_proto_opt=snakeToCamel=true \
   --ts_proto_opt=forceLong=number \
-  -I ${protodir} \
-  ${protofile}
-    `)
+  -I "${protodir}" \
+  "${protofile}"
+    `,{ stdio: 'inherit' })
