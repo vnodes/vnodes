d8acc3913645b9dc83bb1af7dc5a9a77604b90fd
robert-brightline <robert.brightline@gmail.com>
2026-03-30 19:19:58 -0500

    feat: created sample proto file

--- apps/sample-service/scripts/build.mjs
+++ apps/sample-service/scripts/build.mjs
@@ -1,14 +1,25 @@
-// import { execSync } from "node:child_process";
+import { execSync } from "node:child_process";
+import { join } from "node:path";
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 
 console.log('building');
-// execSync(`
-//     protoc \
-//   --plugin=./node_modules/.bin/protoc-gen-ts_proto \
-//   --ts_proto_out=./src/generated \
-//   --ts_proto_opt=nestJs=true \
-//   --ts_proto_opt=fileSuffix=.ts \
-//   --ts_proto_opt=snakeToCamel=true \
-//   --ts_proto_opt=forceLong=number \
-//   -I ./src/proto \
-//   ./src/proto/schema.proto
-//     `)
+const plugin = join('..', 'node_modules/.bin/protoc-gen-ts_proto')
+
+const protodir = join('..', 'proto')
+const protofile = join('..', 'proto', 'sample-service.proto')
+
+execSync(`
+    protoc \
+  --plugin= ${plugin}\
+  --ts_proto_out=./src/generated \
+  --ts_proto_opt=nestJs=true \
+  --ts_proto_opt=fileSuffix=.ts \
+  --ts_proto_opt=snakeToCamel=true \
+  --ts_proto_opt=forceLong=number \
+  -I ${protodir} \
+  ${protofile}
+    `)
