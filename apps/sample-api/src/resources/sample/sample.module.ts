e14ea1c52c42ed1b55f31ec7be1c20e8591e63cb
robert-brightline <robert.brightline@gmail.com>
2026-04-04 20:27:50 -0500

    feat: added tasks

---
+++ apps/sample-api/src/resources/sample/sample.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@vnodes/nestjs/common';
+import { PrismaModule } from '@vnodes/nestjs/prisma';
+import { SampleController } from './sample.controller.js';
+import { SampleService } from './sample.service.js';
+
+@Module({
+    imports: [PrismaModule.forFeature({ models: ['sample'] })],
+    providers: [SampleService],
+    controllers: [SampleController],
+})
+export class SampleModule {}
