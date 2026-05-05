f2f35e0d064e29d710d0b402c8033abce2b2ffe7
robert-brightline <robert.brightline@gmail.com>
2026-05-05 14:54:11 -0500

    feat: created the sitemap generator

--- plugins/plugin/src/sitemap/sitemap.ts
+++ plugins/plugin/src/sitemap/sitemap.ts
@@ -1,13 +1,15 @@
 import * as path from 'node:path';
-import { cwd } from 'node:process';
-import { generateFiles, type Tree } from '@nx/devkit';
+import * as process from 'node:process';
+import { generateFiles, type Tree, workspaceRoot } from '@nx/devkit';
 import type { SitemapGeneratorSchema } from './schema';
 
 export async function sitemapGenerator(tree: Tree, options: SitemapGeneratorSchema) {
-    const lastModDate = new Date().toDateString();
+    const lastModDate = new Date().toISOString().split('T')[0];
     const baseUrl = options.baseUrl ?? 'https://vnodes.github.io';
-    const pages = options.pages ?? ['home', 'about', 'services', 'products'];
-    generateFiles(tree, path.join(__dirname, 'files'), cwd(), {
+    const pages = options.pages
+        ? options.pages.split(',').map((e) => e.trim())
+        : ['home', 'about', 'services', 'products'];
+    generateFiles(tree, path.join(__dirname, 'files'), path.relative(workspaceRoot, process.cwd()), {
         lastModDate,
         baseUrl,
         pages,
