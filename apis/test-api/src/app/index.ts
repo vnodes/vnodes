5984eeb11cccd5e30f5a2cf004eb3ee5b0dc1e44
Robert Brightline <robert.brightline@gmail.com>
2026-06-11 16:31:51 -0500

    feat: configured the barral file

--- apis/test-api/src/app/index.ts
+++
@@ -1,4 +0,0 @@
-// @index(['./**/*.ts', '!./**/*.spec.ts'], f => `export * from '${f.path}.js'`)
-export * from './resources/sample/sample.controller.js';
-export * from './app.module.js';
-export * from './bootstrap.js';
--- apis/test-api/src/main.ts
+++ apis/test-api/src/main.ts
@@ -1,3 +1,3 @@
-import { bootstrap } from './app/index.js';
+import { bootstrap } from './app/bootstrap.js';
 
 bootstrap();
