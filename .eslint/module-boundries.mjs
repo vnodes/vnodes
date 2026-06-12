723ac85fdd7ebd8a3042e2ee484e7d5541e7a2cb
Robert Brightline <robert.brightline@gmail.com>
2026-06-12 16:21:23 -0500

    chore: fixed the workflow

--- .eslint/module-boundries.mjs
+++ .eslint/module-boundries.mjs
@@ -9,33 +9,21 @@ export default [
           allow: ['^.*/eslint(\\.base)?\\.config\\.mjs$'],
           depConstraints: [
             {
-              sourceTag: 'app:*',
+              sourceTag: 'app:api',
               onlyDependOnLibsWithTags: [
-                'lib:core',
                 'lib:module',
                 'lib:shared',
                 'lib:types',
               ],
             },
-            {
-              sourceTag: 'lib:core',
-              onlyDependOnLibsWithTags: [
-                'lib:shared',
-                'lib:types',
-                'lib:module',
-              ],
-            },
-
             {
               sourceTag: 'lib:module',
               onlyDependOnLibsWithTags: [
                 'lib:shared',
                 'lib:types',
                 'lib:module',
-                'lib:core',
               ],
             },
-
             {
               sourceTag: 'lib:shared',
               onlyDependOnLibsWithTags: ['lib:types'],
