95b768fdbb9eda9bb9a4c1e0caf03a48d2c05ecd
Robert Brightline <robert.brightline@gmail.com>
2026-06-16 18:19:39 -0500

    feat: created workspace generator

--- plugins/cli/src/cli/cli.ts
+++ plugins/cli/src/cli/cli.ts
@@ -4,9 +4,10 @@ import { wd } from './wd/wd.js';
 import { suffix } from './suffix/suffix.js';
 import { rm } from './rm/rm.js';
 import { bye } from './bye/bye.js';
+import { workspace } from './workspace/workspace.js';
 
 program.name('Vnodes cli').description('Tools and generators').version('0.1.0').showHelpAfterError(true);
 
-[hello, bye, wd, suffix, rm].map((c) => c(program));
+[hello, bye, wd, suffix, rm, workspace].map((c) => c(program));
 
 program.parse();
