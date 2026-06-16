fdf0f8e239d541e93b965f8d287fcb1428fe97bd
Robert Brightline <robert.brightline@gmail.com>
2026-06-16 14:39:29 -0500

    feat: created the command generator

--- plugins/cli/src/cli/cli.ts
+++ plugins/cli/src/cli/cli.ts
@@ -3,13 +3,10 @@ import { hello } from './hello/hello.js';
 import { wd } from './wd/wd.js';
 import { suffix } from './suffix/suffix.js';
 import { rm } from './rm/rm.js';
+import { bye } from './bye/bye.js';
 
-program
-  .name('Vnodes cli')
-  .description('Tools and generators')
-  .version('0.1.0')
-  .showHelpAfterError(true);
+program.name('Vnodes cli').description('Tools and generators').version('0.1.0').showHelpAfterError(true);
 
-[hello, wd, suffix, rm].map((c) => c(program));
+[hello, bye, wd, suffix, rm].map((c) => c(program));
 
 program.parse();
--- plugins/cli/src/generators/command/command.ts
+++ plugins/cli/src/generators/command/command.ts
@@ -3,12 +3,16 @@ import { formatFiles, generateFiles, names, type Tree } from '@nx/devkit';
 import type { CommandGeneratorSchema } from './schema.js';
 import { fileURLToPath } from 'node:url';
 import { dirname, join } from 'node:path';
+import { inferProjectConfiguration } from '../../utils/infer-project-configuration.js';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export async function commandGenerator(tree: Tree, options: CommandGeneratorSchema) {
   const sourceRoot = join(__dirname, 'files');
-  const targetRoot = `${options.name}`;
+  const projectConfig = inferProjectConfiguration(tree);
+  const projectRoot = projectConfig.root;
+  const targetRoot = `${projectRoot}/src/cli/${options.name}`;
+
   generateFiles(tree, sourceRoot, targetRoot, { ...names(options.name) });
   await formatFiles(tree);
 }
